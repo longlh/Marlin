5456bb849b9121ce3954516ff309746db47d692c
LongLH <i.me.mine@luchoanglong.com>
2019-11-11 18:05:04 +0700

    update PID E0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -379,10 +379,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
+  // HEVO
+  #define DEFAULT_Kp 34.57
+  #define DEFAULT_Ki 3.10
+  #define DEFAULT_Kd 96.28
+
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
