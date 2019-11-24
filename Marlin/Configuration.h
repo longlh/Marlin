2fa695c91e76e0cd1dab05768299d69f31a3d7b8
LongLH <i.me.mine@luchoanglong.com>
2019-11-24 21:01:47 +0700

    switch to LCD2004

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1537,7 +1537,7 @@
 //
 // Note: Usually sold with a white PCB.
 //
-//#define REPRAP_DISCOUNT_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_SMART_CONTROLLER
 
 //
 // ULTIMAKER Controller.
@@ -1664,7 +1664,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+// #define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // ReprapWorld Graphical LCD
